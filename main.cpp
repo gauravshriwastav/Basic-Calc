1454437dcea39a14ec958610da73c9627f2dea1f
Gaurav Shriwastava <56992403+gauravshriwastav@users.noreply.github.com>
2019-10-27 19:38:44 +0545

    Update main.cpp
    Added description and made easy to understand!!

--- main.cpp
+++ main.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 int main()
 {
-    int numberlikh;
-    int dosarnumber;
-    int tesarnumber;
+    //defining numbers
+    int num1;
+    int num2;
+    int num3;
     int resultforaddition;
     int resultformultiplication;
     double resultfordivision;
     int resultforsubtraction;
-    cout<<"numberlikh"<<endl;
-    cin>>numberlikh;
-    cout<<"dosarnumber"<<endl;
-    cin>>dosarnumber;
-    cout<<"tesarnumber";
-    cin>>tesarnumber;
-    resultforaddition = numberlikh+dosarnumber+tesarnumber;
-    resultforsubtraction = (numberlikh+dosarnumber)-tesarnumber;
-    resultfordivision = (numberlikh+dosarnumber)/tesarnumber;
-    resultformultiplication = numberlikh*dosarnumber*tesarnumber;
+    //taking inputs from users
+    cout<<"num1"<<endl; 
+    cin>>num1;
+    cout<<"num2"<<endl;
+    cin>>num2;
+    cout<<"num3";
+    cin>>num3;
+    //defining calculations
+    resultforaddition = num1+num2+num3;
+    resultforsubtraction = (num1+num3)-num1;
+    resultfordivision = (num1+num2)/num3;
+    resultformultiplication = num1*num2*num3;
+    //printing results
     cout<<endl<<endl<<"The addition of above numbers is"<<" "<<resultforaddition<<endl<<endl;
     cout<<endl<<endl<<"The subtraction of above numbers is"<<" "<<resultforsubtraction<<endl<<endl;
     cout<<endl<<endl<<"The multiplication of above numbers is"<<" "<<resultformultiplication<<endl<<endl;
